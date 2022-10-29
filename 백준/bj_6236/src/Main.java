fe3ccbf74a0e395055c9258f90fc7f3abd01a9cf
Yeongjin <yjinlee99@naver.com>
2022-10-29 23:46:46 +0900

    백준 6236 주석

--- 백준/bj_6236/src/Main.java
+++ 백준/bj_6236/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //용돈 사용할 날짜 수
         int n = sc.nextInt();
+        //몇 번 인출할지
         int m = sc.nextInt();
+        //사용할 금액 배열
         int[] money = new int[n];
 
+        //start는 배열의 최대값
         int start = 0;
+        //end는 배열을 다 더한 값
         int end = 0;
         for (int i=0; i<n; i++){
             money[i] = sc.nextInt();
@@ -16,11 +21,13 @@ public class Main {
             end += money[i];
         }
 
+        //매개 변수 탐색
         while(start<=end) {
             int mid = (start + end)/2;
             int check = mid;
             int num = 1;
 
+            //몇 번 인출할지 구하기
             for(int i=0;i<n;i++){
                 check = check - money[i];
                 if(check < 0) {
